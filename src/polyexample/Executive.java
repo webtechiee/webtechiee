1267c240034f937dd7cc12a7dec8187164dd2562
webtechiee <minaj1159@gmail.com>
2023-07-15 10:14:36 -0400

    hello

---
+++ src/polyexample/Executive.java
@@ -0,0 +1,45 @@
+package polyexample;
+
+//Executive.java       Java Foundations
+
+// CST8284 - POLYMORPHISM
+//Represents an executive staff member, who can earn a bonus.
+//********************************************************************
+
+public class Executive extends Employee
+{
+private double bonus;
+
+//-----------------------------------------------------------------
+//  Constructor: Sets up this executive with the specified
+//  information.
+//-----------------------------------------------------------------
+public Executive(String eName, String eAddress, String ePhone,
+                 String socSecNumber, double rate)
+{
+  super(eName, eAddress, ePhone, socSecNumber, rate);
+
+  bonus = 0;  // bonus is yet to be awarded
+}
+
+//-----------------------------------------------------------------
+//  Awards the specified bonus to this executive. Compare this section with the Hourly subclass.
+// What is the difference between both subclasses?
+//-----------------------------------------------------------------
+public void awardBonus(double execBonus)
+{
+  bonus = execBonus;
+}		
+//Computes and returns the pay for an executive, which is the
+//  regular employee payment plus a one-time bonus.
+//-----------------------------------------------------------------
+public double pay()
+{
+  double payment = super.pay() + bonus;
+
+  bonus = 0;
+
+  return payment;
+}
+}		
+
---
+++ src/polyexample/Volunteer.java
@@ -0,0 +1,29 @@
+
+package polyexample;
+
+//Volunteer.java       Java Foundations
+//
+//Represents a staff member that works as a volunteer.
+//********************************************************************
+
+public class Volunteer extends StaffMember
+{
+//-----------------------------------------------------------------
+//  Constructor: Sets up this volunteer using the specified
+//  information.
+//-----------------------------------------------------------------
+public Volunteer(String eName, String eAddress, String ePhone)
+{
+  super(eName, eAddress, ePhone);
+}
+
+//-----------------------------------------------------------------
+//  Returns a zero pay value for this volunteer. Is this the same as for other classes?
+//-----------------------------------------------------------------
+public double pay()
+{
+  return 0.0;
+}
+}
+	
+
